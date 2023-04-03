3662e2d4a82d5f70eda6ef9eba50671842929f51
Hoàng Quốc Trung <103867359+TrungHQ-02@users.noreply.github.com>
2023-04-03 10:35:54 +0700

    Finish export World Data to excel

--- src/components/WorldData.vue
+++ src/components/WorldData.vue
@@ -91,45 +91,114 @@ export default {
     exportToExcel() {
       // Tạo workbook mới
       const workbook = new ExcelJS.Workbook();
-
       const worksheet = workbook.addWorksheet("My Sheet");
-      worksheet.columns = [
-        { header: "Active Cases", key: "active_cases" },
-        { header: "Deaths per 1M Population", key: "deaths_per_1m_population" },
-        { header: "New Cases", key: "new_cases" },
-        { header: "New Deaths", key: "new_deaths" },
-        { header: "Serious Critical", key: "serious_critical" },
-        { header: "Statistic Taken At", key: "statistic_taken_at" },
-        { header: "Total Cases", key: "total_cases" },
-        {
-          header: "Total Cases per 1M Population",
-          key: "total_cases_per_1m_population",
+
+      worksheet.mergeCells("A1:I3");
+      const titleCell = worksheet.getCell("A1");
+      titleCell.value = "Global Report";
+      titleCell.alignment = { vertical: "middle", horizontal: "center" };
+      titleCell.font = {
+        name: "Arial Black",
+        family: 2,
+        size: 38,
+      };
+
+      const now = new Date();
+      const date = now.getDate(); // Lấy ngày trong tháng (1-31)
+      const month = now.getMonth() + 1; // Lấy tháng (0-11). Cộng thêm 1 để lấy tháng từ 1-12
+      const year = now.getFullYear(); // Lấy năm (4 chữ số)
+      const hours = now.getHours(); // Lấy giờ (0-23)
+      const minutes = now.getMinutes(); // Lấy phút (0-59)
+      const seconds = now.getSeconds().toString().padStart(2, "0"); // Lấy giây (0-59) và hiển thị dưới dạng 2 chữ số nếu giá trị là từ 0-9
+
+      worksheet.mergeCells("A4:I5");
+      let timeStampCell = worksheet.getCell("A4");
+      timeStampCell.value =
+        "This data was taken at " +
+        this.worldStatistics.statistic_taken_at +
+        " and downloaded at " +
+        ` ${date}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+      timeStampCell.alignment = { vertical: "middle", horizontal: "center" };
+      timeStampCell.font = {
+        name: "Arial",
+        family: 1,
+        size: 13,
+      };
+
+      worksheet.mergeCells("A6:I9");
+      let desCell = worksheet.getCell("A6");
+      desCell.value =
+        "This table contains the following information related to COVID-19: Active Cases, Deaths per 1M Population, New Cases, New Deaths, Serious Critical cases, Total Cases, Total Cases per 1M Population, Total Deaths, and Total Recovered cases. The data in the table is updated regularly to provide accurate information on the global situation of the pandemic. This information can be used to analyze trends and make informed decisions related to travel, social distancing, and other measures to prevent the spread of COVID-19. By understanding the data in this table, individuals and organizations can take necessary precautions to protect themselves and others from the virus.";
+      desCell.font = {
+        name: "Times New Roman",
+        family: 1,
+        size: 10,
+      };
+
+      desCell.alignment = { wrapText: true };
+
+      // Init table data
+      let table = {
+        name: "MyTable",
+        ref: "A10",
+        headerRow: true,
+        style: {
+          showRowStripes: true,
+          showColumnStripes: true,
         },
-        { header: "Total Deaths", key: "total_deaths" },
-        { header: "Total Recovered", key: "total_recovered" },
-      ];
+        columns: [
+          { name: "Active Cases" },
+          { name: "Deaths per 1M Population" },
+          { name: "New Cases" },
+          { name: "New Deaths" },
+          { name: "Serious Critical" },
+          { name: "Total Cases" },
+          { name: "Total Cases per 1M Population" },
+          { name: "Total Deaths" },
+          { name: "Total Recovered" },
+        ],
+        rows: [
+          [
+            convertToNumber(this.worldStatistics.active_cases),
+            convertToNumber(this.worldStatistics.deaths_per_1m_population),
+            convertToNumber(this.worldStatistics.new_cases),
+            convertToNumber(this.worldStatistics.new_deaths),
+            convertToNumber(this.worldStatistics.serious_critical),
+            convertToNumber(this.worldStatistics.total_cases),
+            convertToNumber(this.worldStatistics.total_cases_per_1m_population),
+            convertToNumber(this.worldStatistics.total_deaths),
+            convertToNumber(this.worldStatistics.total_recovered),
+          ],
+        ],
+      };
+      worksheet.addTable(table);
 
-      worksheet.addRow({
-        active_cases: convertToNumber(this.worldStatistics.active_cases),
-        deaths_per_1m_population: convertToNumber(
-          this.worldStatistics.deaths_per_1m_population
-        ),
-        new_cases: convertToNumber(this.worldStatistics.new_cases),
-        new_deaths: convertToNumber(this.worldStatistics.new_deaths),
-        serious_critical: convertToNumber(
-          this.worldStatistics.serious_critical
-        ),
-        statistic_taken_at: this.worldStatistics.statistic_taken_at,
-        total_cases: convertToNumber(this.worldStatistics.total_cases),
-        total_cases_per_1m_population: convertToNumber(
-          this.worldStatistics.total_cases_per_1m_population
-        ),
-        total_deaths: convertToNumber(this.worldStatistics.total_deaths),
-        total_recovered: convertToNumber(this.worldStatistics.total_recovered),
-      });
+      // adjust column width
+      const columnCount = table.columns.length;
+      for (let i = 1; i <= columnCount; i++) {
+        const column = worksheet.getColumn(i);
+        let maxLength = 0;
 
-      // worksheet.autoFitColumns();
+        column.eachCell({ includeEmpty: true }, function (cell) {
+          if (
+            cell.address[1] != "4" &&
+            cell.address[1] != "5" &&
+            cell.address[1] != "6" &&
+            cell.address[1] != "7" &&
+            cell.address[1] != "8" &&
+            cell.address[1] != "9"
+          ) {
+            const length = cell.value ? cell.value.toString().length : 10;
+            if (length > maxLength) {
+              maxLength = length;
+            }
+          }
+        });
 
+        column.width = maxLength + 2; // add some padding to the width
+      }
+
+      // export file
       const timeStamp = this.worldStatistics.statistic_taken_at;
       workbook.xlsx.writeBuffer().then((buffer) => {
         const blob = new Blob([buffer], {
